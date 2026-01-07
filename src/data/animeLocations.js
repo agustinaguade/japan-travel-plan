8aed0e903a8503126c2008cee7331f58a0caba9f
Agustin Aguade <agustinaguade@gmail.com>
2026-01-07 12:08:38 -0300

    Create animeLocations.js

---
+++ src/data/animeLocations.js
@@ -0,0 +1,37 @@
+export const animeLocations = [
+  {
+    anime: "One Piece",
+    city: "Tokyo / Osaka",
+    color: "#f59e0b",
+    image: "https://source.unsplash.com/1200x700/?tokyo,shibuya,city,night",
+    locations: ["Mugiwara Store Shibuya", "Mugiwara Store Odaiba", "Tokyo Station", "USJ (summer)"],
+  },
+  {
+    anime: "Demon Slayer",
+    city: "Tokyo / Fukuoka",
+    color: "#ef4444",
+    image: "https://source.unsplash.com/1200x700/?asakusa,sensoji,temple,tokyo",
+    locations: ["Asakusa (Muzan scene)", "Ashikaga Wisteria", "Mt. Kumotori", "Kamado Shrine"],
+  },
+  {
+    anime: "Jujutsu Kaisen",
+    city: "Tokyo",
+    color: "#22c55e",
+    image: "https://source.unsplash.com/1200x700/?shibuya,tokyo,neon,crossing",
+    locations: ["Shibuya Station B5F", "Hikarie Building", "Harajuku", "Sendagaya Tunnel"],
+  },
+  {
+    anime: "Samurai X",
+    city: "Kyoto",
+    color: "#a78bfa",
+    image: "https://source.unsplash.com/1200x700/?kyoto,arashiyama,bamboo,temple",
+    locations: ["Arashiyama", "Ikedaya Inn", "Shinsengumi HQ", "Ryozen Museum"],
+  },
+  {
+    anime: "Nintendo / Mario",
+    city: "Osaka / Tokyo",
+    color: "#38bdf8",
+    image: "https://source.unsplash.com/1200x700/?osaka,themepark,lights,night",
+    locations: ["Super Nintendo World", "Nintendo Tokyo", "Pokemon Center Mega", "Nintendo Kyoto"],
+  },
+];
